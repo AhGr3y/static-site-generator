c7dc1a90d62dd8e8675c3a1371e2f00c8b787a7b
ahgr3y <leehh97@gmail.com>
2024-04-05 16:52:19 +0800

    added generate_pages_recursive

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,7 @@
 import os
 
+from pathlib import Path
+
 from htmlnode import (
     HTMLNode,
     ParentNode,
@@ -12,6 +14,29 @@ from block_markdown import (
     block_to_block_type,
     block_type_heading,
 )
+
+def extract_title(md):
+
+    # If md is empty, return empty string
+    if len(md) == 0:
+        return ""
+
+    # Convert md to markdown blocks
+    blocks = markdown_to_blocks(md)
+
+    # Loop through blocks
+    for block in blocks:
+        # Get block type
+        block_type = block_to_block_type(block)
+        # Check if block type is heading
+        if block_type == block_type_heading:
+            # Check if its h1 heading
+            if block.startswith("# "):
+                # Extract text from block and return it
+                return block[2:]
+    
+    # Raise error as no h1 found
+    raise ValueError("Invalid markdown for html")
 
 def generate_page(from_path, template_path, dest_path):
     
@@ -56,25 +81,35 @@ def generate_page(from_path, template_path, dest_path):
         # Write content of new_html to dest_path
         f.write(new_html)
 
-def extract_title(md):
-
-    # If md is empty, return empty string
-    if len(md) == 0:
-        return ""
-
-    # Convert md to markdown blocks
-    blocks = markdown_to_blocks(md)
-
-    # Loop through blocks
-    for block in blocks:
-        # Get block type
-        block_type = block_to_block_type(block)
-        # Check if block type is heading
-        if block_type == block_type_heading:
-            # Check if its h1 heading
-            if block.startswith("# "):
-                # Extract text from block and return it
-                return block[2:]
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     
-    # Raise error as no h1 found
-    raise ValueError("Invalid markdown for html")
+    # If dir_path_content does not exist, raise an error
+    if os.path.exists(dir_path_content) == False:
+        raise ValueError("dir_path_content does not exist!")
+
+    # List items in dir_path_content
+    docs = os.listdir(dir_path_content)
+
+    # Loop through docs
+    for doc in docs:
+        # Get doc path
+        doc_path = os.path.join(dir_path_content, doc)
+        # Check if doc is a file
+        if os.path.isfile(doc_path):
+            # Create Path object with doc
+            doc_path_obj = Path(doc_path)
+            # Get doc suffix
+            doc_suffix = doc_path_obj.suffix
+            # Check if doc is a markdown file
+            if doc_suffix == ".md":
+                # Get path of doc in dest_dir_path
+                doc_dest_path = os.path.join(dest_dir_path, "index.html")
+                # Generate a .html file using doc_dest_path and store it in dest_dir_path
+                generate_page(doc_path, template_path, doc_dest_path)
+        
+        # Check if doc is a directory
+        if os.path.isdir(doc_path):
+            # Get path of doc in dest_dir_path
+            doc_dest_path = os.path.join(dest_dir_path, doc)
+            # Recursively call generate_pages_recursive
+            generate_pages_recursive(doc_path, template_path, doc_dest_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from generate_page import generate_page
+import os
+
+from generate_page import generate_pages_recursive
 
 def main():
-    from_path = "./content/index.md"
+    dir_path_content = "./content"
     template_path = "./template.html"
-    dest_path = "./public/index.html"
-    generate_page(from_path, template_path, dest_path)
+    dest_dir_path = "./public"
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
 
 main()    
